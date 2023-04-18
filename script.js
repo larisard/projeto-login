e078b4dca0f44f7e224870442139530501c97065
Larissa Sardinha <larissaribeirosardinha@gmail.com>
2023-04-18 20:45:28 -0300

    Criando o projeto

---
+++ script.js
@@ -0,0 +1,50 @@
+const usuarioCadastrado = {username:"Larissard",password:"lari123+", cpf:"12345678912"}
+  const usuarioCadastrado2 = {username:"Admin",password:"admin123!", cpf:"12345678921"}
+  const usuarioCadastrado3 = {username:"Usuario",password:"user123!", cpf:"12345678931"}
+
+  const loginBtn = document.querySelector("#log")
+  loginBtn.addEventListener("click", ()=>{
+    const pessoa = new Pessoa(
+      document.getElementsByName("username"),
+      document.getElementsByName("pass"),
+      document.getElementsByName("cpf")
+    );
+    pessoa.login()
+  })
+
+  const logadoTela = document.querySelector(".logado")
+  const loginTela = document.querySelector(".login-tela")
+
+
+class Pessoa {
+  constructor(username, password, cpf) {
+    this.username = username[0].value;
+    this.password = password[0].value;
+    this.cpf = cpf[0].value;
+  }
+
+  loginAux(){
+    window.location.href = "home.html"
+    
+  }
+  login() {
+    const user = { username : this.username, password: this.password, cpf: this.cpf};
+    console.log("no login, ", user)
+    if(user.username==usuarioCadastrado.username && user.password==usuarioCadastrado.password && user.cpf==usuarioCadastrado.cpf){
+     this.loginAux()
+    }
+    else if(user.username==usuarioCadastrado2.username && user.password==usuarioCadastrado2.password && user.cpf==usuarioCadastrado2.cpf){
+      this.loginAux()
+    }
+    else if(user.username==usuarioCadastrado3.username && user.password==usuarioCadastrado3.password && user.cpf==usuarioCadastrado3.cpf){
+      this.loginAux()
+    }
+    else {
+    
+      alert("Usuário não cadastrado")
+    }
+
+  }
+  
+
+}
